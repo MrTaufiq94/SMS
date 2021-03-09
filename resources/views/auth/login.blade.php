5a8706d19c37e9ac7fe5e5b6121d784f13a80fb3
MrTaufiq94 <m.taufiqfariduddin94@gmail.com>
2021-03-09 09:17:08 +0800

    modified login with add asset for styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,68 @@
 @extends('layouts.auth', ['title' => 'Login'])
 
 @section('content')
-
-    <div class="container">
-
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-
-            <div class="col-md-4">
-                <div class="img-logo text-center mt-5">
-                    <img src="{{ asset('assets/img/company.png') }}"
-                        style="width: 80px;">
-                </div>
-                <div class="card o-hidden border-0 shadow-lg mb-3 mt-5">
-                    <div class="card-body p-4">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        <div class="text-center">
-                            <h1 class="h5 text-gray-900 mb-3">LOGIN ADMIN</h1>
-                        </div>
-
-                        <form action="{{ route('login') }}" method="POST">
-                            @csrf
-                            <div class="form-group">
-                                <label class="font-weight-bold text-uppercase">Email address</label>
-                                <input type="email" name="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" placeholder="Masukkan Alamat Email">
-                                @error('email')
-                                    <div class="alert alert-danger mt-2">
-                                        {{ $message }}
-                                    </div>    
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <label class="font-weight-bold text-uppercase">Password</label>
-                                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Masukkan Password">
-                                @error('password')
-                                    <div class="alert alert-danger mt-2">
-                                        {{ $message }}
-                                    </div>    
-                                @enderror
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-block">LOGIN</button>
-                            <hr>
-            
-                            <a href="/forgot-password">Lupa Password ?</a>
-            
-                        </form>
-
-                    </div>
-                </div>
-            </div>
-
+<div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+            <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
         </div>
-
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <p class="login-box-msg">Sign in to start your session</p>
+    
+            <form action="{{ route('login') }}" method="POST">
+                @csrf
+                <div class="input-group mb-3">
+                    <input type="email" name="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" placeholder="Email">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                    @error('email')
+                        <span class="error invalid-feedback">{{ $message }}</span>   
+                    @enderror
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                    @error('password')
+                        <span class="error invalid-feedback">{{ $message }}</span>   
+                    @enderror
+                </div>
+                <div class="row">
+                    <div class="col-8">
+                        <div class="icheck-primary">
+                            <input type="checkbox" id="remember">
+                            <label for="remember">
+                            Remember Me
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+            <p class="mb-1">
+                <a href="/forgot-password">I forgot my password</a>
+            </p>
+        </div>
+        <!-- /.card-body -->
     </div>
+    <!-- /.card -->
+</div>
+<!-- /.login-box -->
 
 @endsection
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,28 +8,26 @@
     <meta name="author" content="">
     <link rel="shortcut icon" href="{{ asset('assets/img/company.png') }}" type="image/x-icon">
     <title>{{ $title ?? config('app.name') }} - Admin</title>
-    <!-- Custom fonts for this template-->
-    <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-    <!-- Custom styles for this template-->
-    <link href="{{ asset('assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('assets/css/adminlte.min.css') }}">
 </head>
 
-<body style="background-color: #e2e8f0;">
+<body class="hold-transition login-page">
 
     @yield('content')
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="{{ asset('assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="{{ asset('assets/js/sb-admin-2.min.js') }}"></script>
+    <!-- jQuery -->
+    <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('assets/js/adminlte.min.js') }}"></script>
 
 </body>
 </html>
